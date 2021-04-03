af1326ec7723e12e63a07e29559429d2a8b7599a
pengkai <kylepeng93@gmail.com>
2021-04-03 19:50:57 +0800

    将git仓库的url指向了自己的仓库

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,7 +12,7 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/kylepeng93/spring-boot-api-example.git'
                     }
                     branch 'master'
                 }
